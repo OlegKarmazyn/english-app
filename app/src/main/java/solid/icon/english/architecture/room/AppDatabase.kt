c09580488478bd874ac92a8341b20ac292e3f415
OlegKarmazyn <ol0321k@gmail.com>
2023-01-28 22:44:25 +0200

    subTopicDao

--- app/src/main/java/solid/icon/english/architecture/room/AppDatabase.kt
+++ app/src/main/java/solid/icon/english/architecture/room/AppDatabase.kt
@@ -1,21 +1,23 @@
 package solid.icon.english.architecture.room
 
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
 @Database(
     entities = [WordModel::class, TopicModel::class],
-    version = 1,
+    version = 2,
     exportSchema = true,
-//    autoMigrations = [
-//        AutoMigration(
-//            from = 1,
-//            to = 2
-//        )
-//    ]
+    autoMigrations = [
+        AutoMigration(
+            from = 1,
+            to = 2
+        )
+    ]
 )
 
 abstract class AppDatabase : RoomDatabase() {
     abstract fun wordModelDao(): WordModelDao?
     abstract fun topicModelDao(): TopicModelDao?
+    abstract fun subTopicDao(): SubTopicDao?
 }
